a5ae730edc10ff89d014cdbbd648861a0ee46a47
ljz3 <kevin.ottawa@hotmail.com>
2021-01-10 01:15:18 -0500

    Update deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const {interface, bytecode} = require('./compile');
+const compiledFactory = require('./build/CampaignFactory.json')
 
 const provider = new HDWalletProvider(
     'INSERT MNEMONIC PHRASE HERE',
@@ -13,11 +13,10 @@ const deploy = async () => {
 
     const accounts = await web3.eth.getAccounts();
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: '0x' + bytecode}) // add 0x bytecode
-        .send({from: accounts[0]}); // remove 'gas'
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
 
-    console.log(interface);
     console.log('Contract deployed to', result.options.address);
 };
 deploy();
