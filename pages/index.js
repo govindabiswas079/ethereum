ffd642634c4d2f9283c1505a44995afc782d74cb
ljz3 <kevin.ottawa@hotmail.com>
2021-05-04 17:14:34 -0400

    Added react semantic ui as dependency

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import factory from '../ethereum/factory';
 class CampaignIndex extends Component{
 
     static async getInitialProps(){
-        const campaigns = await factory.methods.getDepoyedCampaigns().call();
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        return {campaigns};
+        return { campaigns };
     }
 
     render(){
-        return <div>Campaigns Index</div>;
+        return <div>{this.props.campaigns[0]}</div>;
     }
 }
 
