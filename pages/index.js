83a90763cc87ef61f84887cb0cc7e0cfdcaa3641
ljz3 <kevin.ottawa@hotmail.com>
2021-05-04 23:23:05 -0400

    Added default display

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Card, Button} from 'semantic-ui-react'
 import factory from '../ethereum/factory';
 
 
@@ -10,8 +11,25 @@ class CampaignIndex extends Component{
         return { campaigns };
     }
 
+    renderCampaigns(){
+        const items = this.props.campaigns.map(address =>{
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+        return <Card.Group items={items}/>;
+    }
+
+
     render(){
-        return <div>{this.props.campaigns[0]}</div>;
+        return ( <div>
+            <link rel="stylesheet"   href="//cdn.jsdelivr.net/npm/semantic-ui@2.0.0/dist/semantic.min.css"/>
+            {this.renderCampaigns()}
+            <Button content="Create Campaign" icon="add circle" primary/>
+        
+        </div>);
     }
 }
 
