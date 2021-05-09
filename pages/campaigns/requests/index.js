56bd3133f7b62e89cb6c9a61d14dc85422d79fda
ljz3 <kevin.ottawa@hotmail.com>
2021-05-09 19:41:56 -0400

    Added routing to the view requests page

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+class RequestIndex extends Component{
+
+
+    render(){
+        return ( 
+        
+        
+            <div>
+                <h3>Requests</h3>
+            </div>
+        
+        );
+    }
+}
+
+export default RequestIndex;
+
