cfc580b3e2b7325f3bfedf4e35e45d8c963b6593
ljz3 <kevin.ottawa@hotmail.com>
2021-05-09 19:30:38 -0400

    Added loading and error messaging in contribute form

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -6,13 +6,17 @@ import {Router} from '../routes';
 
 class ContributionForm extends Component{
     state = {
-        value:''
+        value:'',
+        errorMessage: '',
+        loading: false
     };
 
 
     onSubmit = async (event) => {
         event.preventDefault();
         const campaign = Campaign(this.props.address);
+
+        this.setState({loading: true, errorMessage:''});
 
         try{
             const accounts = await web3.eth.getAccounts();
@@ -23,13 +27,14 @@ class ContributionForm extends Component{
             });
             Router.replaceRoute(`/campaigns/${this.props.address}`);
         }catch(err){
-
+            this.setState({errorMessage: err.message});
         }
+        this.setState({loading: false, value:''});
     };
 
     render(){
         return(
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Contribution Amount</label>
                     <Input
@@ -39,7 +44,12 @@ class ContributionForm extends Component{
                         labelPosition="right"
                     />
                 </Form.Field>
-                <Button primary>Submit</Button>
+                <Message
+                    error
+                    header='There was some errors with your submission'
+                    content={this.state.errorMessage}
+                />
+                <Button primary loading={this.state.loading}>Submit</Button>
             </Form>
         );
     }
